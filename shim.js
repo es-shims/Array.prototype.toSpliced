c4a807f28668763cd255b157ba2afffc613b6de1
Jordan Harband <ljharb@gmail.com>
2022-08-14 21:50:07 -0700

    [New] `shim`/`auto`: add `toSpliced` to `Symbol.unscopables`

--- shim.js
+++ shim.js
@@ -1,14 +1,20 @@
 'use strict';
 
 var define = require('define-properties');
+var shimUnscopables = require('es-shim-unscopables');
+
 var getPolyfill = require('./polyfill');
 
 module.exports = function shim() {
 	var polyfill = getPolyfill();
+
 	define(
 		Array.prototype,
 		{ toSpliced: polyfill },
 		{ toSpliced: function () { return Array.prototype.toSpliced !== polyfill; } }
 	);
+
+	shimUnscopables('toSpliced');
+
 	return polyfill;
 };
